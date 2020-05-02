bcc8e6c72f7c0fd28bc1658fd1d88ad46171bc4c
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-05-02 14:52:35 +0800

    Add booleans

---
+++ 02-intro-to-programming/03-booleans/booleans.rb
@@ -0,0 +1,17 @@
+# Boolean Data Type Lecture
+
+puts true
+# puts false
+puts !true # false
+puts !false # true
+
+
+puts false && false # false
+puts false && true # false
+puts true &&  false # false
+puts true &&  true # true
+
+puts false || false # false
+puts false || true # true
+puts true ||  false # true
+puts true ||  true # true
--- 02-intro-to-programming/03-booleans/exercises/booleans-exercise.rb
+++ 02-intro-to-programming/03-booleans/exercises/booleans-exercise.rb
@@ -3,7 +3,7 @@
 # Print out the result of your expressions using puts.
 # Be sure to test your work by running your code!
 
-puts !true
-puts true && true
-puts false || true
-puts !false || false
+true && true
+true || false
+9 >= 6 && true
+!true || false
