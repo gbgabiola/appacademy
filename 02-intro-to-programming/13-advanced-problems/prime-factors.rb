5e6f9ead3e8f0a500ff253ccdef1b710a659bd1f
Genesis Gabiola <genesisbritanicogabiola@gmail.com>
2020-05-21 22:48:24 +0800

    Update advanced exercises

--- 02-intro-to-programming/13-advanced-problems/prime-factors.rb
+++ 02-intro-to-programming/13-advanced-problems/prime-factors.rb
@@ -1,2 +1,24 @@
 # Write a method prime_factors that takes in a number and returns an array containing all of the prime factors of the given number.
 
+def prime_factors(num)
+  factors = []
+
+  (1..num).each do |i|
+    if num % i == 0 && prime?(i)
+      factors << i
+    end
+  end
+
+  return factors
+end
+
+def prime?(num)
+  return false if num < 2
+  (2...num).each { |i| return false if num % i == 0 }
+  return true
+end
+
+print prime_factors(24) #=> [2, 3]
+puts
+print prime_factors(60) #=> [2, 3, 5]
+puts
